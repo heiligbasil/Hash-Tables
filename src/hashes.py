14c450c5d040abe170d21403393ddc5ee7c6f256
heiligbasil <basilief@yahoo.com>
2020-03-09 12:52:34 -0700

    Another for loop and made the variables consistent

--- src/hashes.py
+++ src/hashes.py
@@ -1,21 +1,25 @@
 import hashlib
 
 n = 1
-key = b'my_value'
+key1 = b'my_value'
 key2 = 'string'.encode()
 key3 = b'lunchtime'
 
 for i in range(n):
-    print(hash(key))
-    print(hashlib.sha3_256(key).hexdigest())
+    print(hash(key1))
+    print(hashlib.sha3_256(key1).hexdigest())
 
 for i in range(n):
     print(hash(key2))
-    print(hashlib.sha3_256(key).hexdigest())
+    print(hashlib.sha3_256(key2).hexdigest())
 
-index = hash(key) % 8
+for i in range(n):
+    print(hash(key3))
+    print(hashlib.sha3_256(key3).hexdigest())
+
+index1 = hash(key1) % 8
 index2 = hash(key2) % 8
 index3 = hash(key3) % 8
-print(index)
+print(index1)
 print(index2)
 print(index3)
